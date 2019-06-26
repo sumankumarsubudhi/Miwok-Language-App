9cfa3aa9f298d50efb7a6554cbf6fec99061a64b
Suman Kumar Subudhi <sonu61866@gmail.com>
2019-06-27 01:46:17 +0530

    Create SimpleFragmentPagerAdapter.java

---
+++ app/src/main/java/com/example/miwok/SimpleFragmentPagerAdapter.java
@@ -0,0 +1,58 @@
+package com.example.sumankumarsubudhi.miwok;
+
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+
+public class SimpleFragmentPagerAdapter extends FragmentPagerAdapter {
+
+
+
+    private String tabTitles[] = new String[] { "Numbers", "Family", "Colors","Phrases" };
+    private Context mContext;
+
+
+
+    public SimpleFragmentPagerAdapter(FragmentManager fm,Context context) {
+        super(fm);
+        mContext = context;
+
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return new NumbersFragment();
+        } else if (position == 1) {
+            return new FamilyFragment();
+        } else if (position == 2) {
+            return new ColorsFragment();
+        } else {
+            return new PhrasesFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position == 0) {
+            return mContext.getString(R.string.category_numbers);
+        } else if (position == 1) {
+            return mContext.getString(R.string.category_family);
+        } else if (position == 2) {
+            return mContext.getString(R.string.category_colors);
+        } else {
+            return mContext.getString(R.string.category_phrases);
+        }
+    }
+
+
+}
